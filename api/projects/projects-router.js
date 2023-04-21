93215c3877219c0089f47794b4666957fe6dcd3f
Veysel Erdemci <v.erdemci@gmail.com>
2023-04-21 22:07:39 +0300

    Sprint Challenge Talimatları

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,56 @@
 // "project" routerını buraya yazın!
+const router = require("express").Router();
+
+const projectsModel = require("./projects-model");
+const mw = require("./projects-middleware");
+
+router.get("/",async(req,res,next)=>{
+    try {
+        const allProjects = await projectsModel.get();
+        res.json(allProjects);
+    } catch (error) {
+        next(error);
+    }
+});
+router.get("/:id",mw.checkProjectId,(req,res,next)=>{
+    try {
+        res.json(req.project);
+    } catch (error) {
+        next(error);
+    }
+});
+router.post("/",mw.checkProjectPayload,async(req,res,next)=>{
+    try {
+        let {validProject} = req;
+        const insertedProject = await projectsModel.insert(validProject);
+        res.status(201).json(insertedProject);
+    } catch (error) {
+        next(error);
+    }
+});
+router.put("/:id",mw.checkProjectId,mw.checkProjectPayload,async(req,res,next)=>{
+    try {
+        const updatedProject = await projectsModel.update(req.params.id,req.validProject);
+        res.json(updatedProject);
+    } catch (error) {
+        next(error);
+    }
+});
+router.delete("/:id",mw.checkProjectId,async (req,res,next)=>{
+    try {
+        await projectsModel.remove(req.params.id);
+        res.json({message:`${req.params.id} ID'li kayıt silindi`});
+    } catch (error) {
+        next(error);
+    }
+});
+router.get("/:id/actions",mw.checkProjectId,async(req,res,next)=>{
+    try {
+        const actionList = await projectsModel.getProjectActions(req.params.id);
+        res.json(actionList);
+    } catch (error) {
+        next(error);
+    }
+});
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -5,5 +5,13 @@ const server = express();
 // Eylem routerınızı /api/actions/actions-router.js içinde oluşturun
 // Proje roterlarınızı /api/projects/projects-router.js içinde oluşturun
 // Bu dosyanın içinde `server.listen()` YAPMAYIN!
+server.use(express.json());//json formatlı isteklerin desteklenmesi için.
+
+const projectsRouter = require("./projects/projects-router");
+const actionsRouter = require("./actions/actions-router");
+
+server.use("/api/projects",projectsRouter);
+server.use("/api/actions",actionsRouter);
+
 
 module.exports = server;
